8602129e1100c58a1e3d616c853b775c9cc98595
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2024-05-24 21:17:59 +0700

    second run

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.7778110944527736
-exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.6500000000000001, min_samples_leaf=12, min_samples_split=2, n_estimators=100)
+# Average CV score on the training set was: 0.770944527736132
+exported_pipeline = ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.8, min_samples_leaf=15, min_samples_split=15, n_estimators=100)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
