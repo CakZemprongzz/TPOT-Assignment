f9a7a14ee26c1fed8f43c27fecd40dbec35fa7bc
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2024-05-25 10:05:35 +0700

    third run  + changes on variable name

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MinMaxScaler
+from tpot.builtins import StackingEstimator, ZeroCount
+from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,11 +14,16 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.770944527736132
-exported_pipeline = ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.8, min_samples_leaf=15, min_samples_split=15, n_estimators=100)
-# Fix random state in exported estimator
-if hasattr(exported_pipeline, 'random_state'):
-    setattr(exported_pipeline, 'random_state', 42)
+# Average CV score on the training set was: 0.791485986250661
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=KNeighborsClassifier(n_neighbors=22, p=1, weights="uniform")),
+    StackingEstimator(estimator=KNeighborsClassifier(n_neighbors=22, p=1, weights="uniform")),
+    MinMaxScaler(),
+    ZeroCount(),
+    GaussianNB()
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
