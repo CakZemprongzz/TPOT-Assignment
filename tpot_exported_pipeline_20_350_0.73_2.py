9c9a6949935150c374389e98acbbd6042675dec2
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2024-06-06 19:00:56 +0700

    Another run

--- tpot_exported_pipeline_20_350_0.73_2.py
+++ tpot_exported_pipeline_20_350_0.73_2.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator, ZeroCount
 from tpot.export_utils import set_param_recursive
@@ -13,11 +13,12 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.8078265468006347
+# Average CV score on the training set was: 0.7981755684822845
 exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=MultinomialNB(alpha=1.0, fit_prior=False)),
+    StackingEstimator(estimator=BernoulliNB(alpha=0.01, fit_prior=False)),
     ZeroCount(),
-    StackingEstimator(estimator=KNeighborsClassifier(n_neighbors=24, p=1, weights="uniform")),
-    RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.1, min_samples_leaf=4, min_samples_split=19, n_estimators=100)
+    LogisticRegression(C=10.0, dual=False, penalty="l2")
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
